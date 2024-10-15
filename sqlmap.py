bb0eeaba2b80c5755e96d53af1f1b95d1f162ff5
cypherdavy <166112437+cypherdavy@users.noreply.github.com>
2024-10-16 00:12:36 +0530

    Delete sqlmap.py

--- sqlmap.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def sql_injection_scan(url):
-    command = f"sqlmap -u {url} --batch"
-    os.system(command)
-
-if __name__ == "__main__":
-    target_url = input("Enter the target URL for SQL Injection scan: ")
-    sql_injection_scan(target_url)
