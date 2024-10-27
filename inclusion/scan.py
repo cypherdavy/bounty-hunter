6936305cdb5465ab33a873f1d549e26057b01222
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2024-10-28 02:23:41 +0530

    Update scan.py

--- inclusion/scan.py
+++ inclusion/scan.py
@@ -1,1 +1,15 @@
+from inclusion import scan
 
+def print_vulnerability(test_url):
+    print(f"Inclusion vulnerability found : {test_url}")
+
+def scan_inclusion(target_url):
+    vulnerabilities = scan(target_url, callback=print_vulnerability)
+
+    with open("inclusion_results.txt", "w") as f:
+        if vulnerabilities:
+            for vulnerability in vulnerabilities:
+                f.write(f"Inclusion vulnerability found : {vulnerability}\n")
+        else:
+            f.write("No vulnerabilities found.\n")
+            print("No vulnerabilities found.")
